90fdec5eaa1217f9009c262ad7bd8bf192b558f1
Aryan Gupta <sanataniaryan010@gmail.com>
2025-06-24 23:37:27 +0530

    Fix: Update psycopg to latest version 3.2.9

--- build.sh
+++ build.sh
@@ -5,17 +5,11 @@ set -o errexit
 # Upgrade pip and setuptools
 pip install --upgrade pip setuptools wheel
 
-# Install psycopg adapter first
-pip install --no-cache-dir psycopg[binary]==3.1.18
-
 # Install requirements with no cache to avoid conflicts
 pip install --no-cache-dir -r requirements.txt
 
-# Skip collectstatic if running locally
-if [ "$RENDER" == "true" ]; then
-  # Collect static files
-  python manage.py collectstatic --no-input
-  
-  # Run migrations
-  python manage.py migrate
-fi
+# Collect static files
+python manage.py collectstatic --no-input
+
+# Run migrations
+python manage.py migrate
