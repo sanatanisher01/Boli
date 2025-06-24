188c944beb2947b7be04047347adaa22eaf0fe43
Aryan Gupta <sanataniaryan010@gmail.com>
2025-06-24 22:59:15 +0530

    Fix Python 3.13 compatibility: Update Pillow and build process

--- build.sh
+++ build.sh
@@ -2,11 +2,11 @@
 # exit on error
 set -o errexit
 
-# Upgrade pip
-pip install --upgrade pip
+# Upgrade pip and setuptools
+pip install --upgrade pip setuptools wheel
 
-# Install requirements
-pip install -r requirements.txt
+# Install requirements with no cache to avoid conflicts
+pip install --no-cache-dir -r requirements.txt
 
 # Collect static files
 python manage.py collectstatic --no-input
